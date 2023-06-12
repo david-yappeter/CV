4700557b4737d480cf9d9a8e8e14f7bf448b087b
David Yappeter <davidyap11les@gmail.com>
2023-06-12 20:46:23 +0700

    feat: update cv content

--- pdf-gen.js
+++ pdf-gen.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const html_to_pdf = require("html-pdf-node");
-const ejs = require("ejs");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const html_to_pdf = require('html-pdf-node');
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 const template = fs.readFileSync(
-  path.resolve(__dirname, "./index.html"),
-  "utf8"
+  path.resolve(__dirname, './index.html'),
+  'utf8'
 );
-const content = ejs.render(template, { title: "Awesome title!" });
-fs.writeFile(path.resolve(__dirname, "./public/index.html"), content, () => {
-  app.use(express.static("dist"));
+const content = ejs.render(template, { title: 'Awesome title!' });
+fs.writeFile(path.resolve(__dirname, './public/index.html'), content, () => {
+  app.use(express.static('dist'));
 
   const server = app.listen(port, async () => {
     const url = `http://localhost:${port}`;
     const options = {
       // format: "A4", // width or height will be override
-      width: "800px",
-      height: "2500px",
-      path: "my-cv.pdf",
+      width: '800px',
+      height: '2660px',
+      path: 'my-cv.pdf',
       printBackground: true, // to apply background colour
     };
     const file = { url };
